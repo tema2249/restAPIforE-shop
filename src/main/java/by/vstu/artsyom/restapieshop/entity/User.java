9f84ae4881a6d8f6f0006a4567d733711da9b6fe
tema2249 <tem2249@gmail.com>
2023-05-26 17:40:38 +0300

    54

--- src/main/java/by/vstu/artsyom/restapieshop/entity/User.java
+++ src/main/java/by/vstu/artsyom/restapieshop/entity/User.java
@@ -1,7 +1,13 @@
 package by.vstu.artsyom.restapieshop.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private int login;
 }
